e4715c84dade433633776bd6b1fe55db44d9fe90
dung1004 <1004nguyenthanhdung@gmail.com>
2020-09-05 17:12:48 +0700

    edit button action and save, show polyline

--- src/components/draw/draw_line.jsx
+++ src/components/draw/draw_line.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom'
+import { IconButton, Button, Tooltip } from "@material-ui/core"
 import { withStyles } from "@material-ui/core/styles";
+import ShowChartIcon from '@material-ui/icons/ShowChart';
 
 const style = {
   root: {
@@ -7,16 +10,21 @@ const style = {
     height: "100vh",
   },
   panel: {
-    width: 200,
     fontFamily: "Arial, sans-serif",
     fontSize: 13,
     position: "absolute",
-    top: 10,
-    left: "60%",
+    top: 0,
+    left: "55%",
     zIndex: 10,
   },
+  button: {
+    margin: 5
+  },
   btnRemove: {
-    cursor: "pointer"
+    cursor: "pointer",
+  },
+  boxButton: {
+    display: "flex"
   }
 };
 
@@ -24,9 +32,8 @@ class DrawLine extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawingManager: "",
+      drawingManager: null,
       shapeSelected: null,
-      colorButtons: {},
       shapeClick: false,
       dataShape: JSON.parse(localStorage.getItem("dataShape")) || [],
       dataOptions: [
@@ -39,15 +46,30 @@ class DrawLine extends Component {
     };
   }
   componentDidMount() {
-    this.init();
+    let _this = this;
+    window.addEventListener("load", function () {
+      _this.init();
+    });
+  }
+
+  componentDidUpdate() {
+    let { drawingManager, dataShape } = this.state;
+    let _this = this;
+    if (drawingManager) {
+      window.google.maps.event.addListener(
+        drawingManager,
+        "overlaycomplete",
+        (e) => _this.handleFinishDrawShape(e, drawingManager, dataShape)
+      );
+    }
   }
 
   init = () => {
-    let { drawingManager, dataShape } = this.state;
+    let { drawingManager } = this.state;
     var map = new window.google.maps.Map(document.getElementById("map"), {
       zoom: 13,
       center: new window.google.maps.LatLng(16.059506, 108.219112),
-      mapTypeId: window.google.maps.MapTypeId.SATELLITE,
+      // mapTypeId: window.google.maps.MapTypeId.SATELLITE,
     });
 
     drawingManager = new window.google.maps.drawing.DrawingManager({
@@ -58,88 +80,103 @@ class DrawLine extends Component {
 
       polylineOptions: {
         editable: true,
-        draggable: true,
       },
       map: map,
     });
 
-    this.setState({
-      ...this.state,
-      drawingManager,
-    }, () => this.buildColorPalette() );
-
-    let _this = this;
+    this.setState(
+      {
+        ...this.state,
+        drawingManager,
+      },
+      () => this.buildColorPalette()
+    );
 
     window.google.maps.event.addListener(
       drawingManager,
-      "overlaycomplete",
-      function (e) {
-        let newShape = e.overlay;
-        newShape.type = e.type;
-
-        drawingManager.setDrawingMode(null);
-
-        let paths = []
-        let pathArray = newShape.getPath().i;
-        pathArray.forEach(function(path) {
-          paths.push({ lat: path.lat(), lng: path.lng() })
-        })
-
-        let objectShape = {
-          path: paths,
-          color: newShape.strokeColor,
-          name: newShape.name,
-          type: newShape.type
-        }
-
-        dataShape = [...dataShape, objectShape]
-        localStorage.setItem("dataShape", JSON.stringify(dataShape))
-
-        console.log('path', paths)
-        console.log('objectShape', objectShape)
-        console.log('newShape', newShape)
-        window.google.maps.event.addListener(newShape, "click", function (e) {
-          _this.handleShapeSelected(newShape);
-
-          _this.setState({
-            ..._this.state,
-            shapeClick: true
-          })
-          // console.log('e click', e.vertex)
-        });
-
-        _this.handleShapeSelected(newShape)
-      }
+      "drawingmode_changed",
+      this.clearShapeSelected
+    );
+    window.google.maps.event.addListener(map, "click", this.clearShapeSelected);
+    window.google.maps.event.addDomListener(
+      document.getElementById("delete-button"),
+      "click",
+      this.handleDeleteShapeSelected
     );
 
-    window.google.maps.event.addListener(drawingManager, 'drawingmode_changed', this.clearShapeSelected);
-    window.google.maps.event.addListener(map, 'click', this.clearShapeSelected);
-    window.google.maps.event.addDomListener(document.getElementById('delete-button'), 'click', this.handleDeleteShapeSelected);
-
-
-    this.loadData(drawingManager, map)
-
+    this.loadData(drawingManager, map);
   };
 
   loadData = (drawingManager, map) => {
-    let { dataShape } = this.state
-    let _this = this
+    let { dataShape } = this.state;
 
-    dataShape.map(data => {
+    console.log('data', dataShape)
+    let _this = this;
+
+    dataShape.map((data) => {
       let polyline = new window.google.maps.Polyline({
         path: data.path,
         strokeColor: data.color,
-        strokeWeight: 3
-      }) 
+        strokeWeight: 4,
+        id: data.id,
+      });
 
-      window.google.maps.event.addListener(polyline, "click", function(e) {
-        _this.handleShapeSelected(polyline);
-      })
+      window.google.maps.event.addListener(polyline, "click", () =>
+        _this.handleShapeSelected(polyline)
+      );
 
-      polyline.setMap(map)
-    })
-   
-  }
+      window.google.maps.event.addListener(polyline.getPath(), "set_at", () =>
+        _this.handleEditableShape(polyline, dataShape)
+      );
+
+      polyline.setMap(map);
+    });
+  };
+
+  handleFinishDrawShape = (e, drawingManager, dataShape) => {
+    let _this = this
+    let id = this.randomID();
+    let newShape = e.overlay;
+    newShape.type = e.type;
+    newShape.id = id;
+
+    drawingManager.setDrawingMode(null);
+
+    let paths = this.handleParsePaths(newShape.type, newShape);
+
+    let objectShape = {
+      id,
+      path: paths,
+      color: newShape.strokeColor,
+      name: newShape.name,
+      type: newShape.type,
+    };
+
+    dataShape = [...dataShape, objectShape];
+
+    this.setState({
+      ...this.state,
+      dataShape,
+    });
+    localStorage.setItem("dataShape", JSON.stringify(dataShape));
+
+    window.google.maps.event.addListener(newShape, "click", function (e) {
+      _this.handleShapeSelected(newShape);
+
+      _this.setState({
+        ..._this.state,
+        shapeClick: true,
+      });
+    });
+
+    if(newShape.type === 'polyline') {
+      window.google.maps.event.addListener(newShape.getPath(), "set_at", () =>
+        this.handleEditableShape(newShape, dataShape)
+      );
+    }
+
+    this.handleShapeSelected(newShape);
+  };
 
   clearShapeSelected = () => {
     let { shapeSelected } = this.state;
@@ -149,7 +186,7 @@ class DrawLine extends Component {
       this.setState({
         ...this.state,
         shapeClick: false,
-        shapeSelected: null
+        shapeSelected: null,
       });
     }
   };
@@ -178,72 +215,146 @@ class DrawLine extends Component {
     }
   };
 
-  handleSelectColor = (color) => {
-    let { dataOptions, colorButtons, drawingManager } = this.state;
-
-
-    dataOptions.map((data, index) => {
-      let currColor = data.color;
-      colorButtons[currColor].style.border =
-        currColor === color ? "5px solid white" : "5px solid transparent";
-    });
-
-    let dataActive = dataOptions.filter(data => data.color === color)
-    // console.log(dataActive)
-
-    let polylineOptions = drawingManager.get('polylineOptions');
-    polylineOptions.strokeColor = color;
-    polylineOptions.name = dataActive[0].name
-    drawingManager.set('polylineOptions', polylineOptions);
-    
-  };
-
-  makeColorButton = (color) => {
-    let _this = this;
-    let span = document.createElement("span");
-    span.className = "color_button";
-    span.style.cssText = `background-color: ${color}; width: 15px; height: 15px; cursor: pointer; display: inline-block   `;
-    window.google.maps.event.addDomListener(span, "click", function () {
+  handleRef = (ref) => {
+    let _this = this
+    window.google.maps.event.addDomListener(ref, "click", function () {
+      let color = ref.getAttribute('data-color')
       _this.handleSelectColor(color);
       _this.handleShapeSelectedColor(color);
     });
+  }
 
-    return span;
+  handleSelectColor = (color) => {
+    let { dataOptions, drawingManager } = this.state;
+
+    dataOptions.map((data, index) => {
+      
+      if(data.color === color) {
+        let buttons = document.getElementsByClassName("button-color")
+        let icons = document.querySelectorAll("button.button-color span svg")
+        
+        let buttonElement = document.querySelector(`[data-color="${color}"]`)
+        let iconElement = document.querySelector(`[data-color="${color}"] span svg`)
+
+
+        for(let b = 0; b < buttons.length; b++) {
+          buttons[b].style.cssText = `background-color: white !important; border-color: ${buttons[b].getAttribute('data-color')}`
+          
+        }
+
+        for(let i = 0; i < icons.length; i++) {
+          icons[i].style.cssText = `color: ${buttons[i].getAttribute('data-color')} !important;`
+        }
+      
+        buttonElement.style.cssText = `background-color: ${color} !important;`
+        iconElement.style.cssText = `color: white;`
+      }
+    });
+
+    let buttonActive = dataOptions.filter((data) => data.color === color);
+
+    let polylineOptions = drawingManager.get("polylineOptions");
+    polylineOptions.strokeColor = color;
+    polylineOptions.name = buttonActive[0].name;
+    drawingManager.set("polylineOptions", polylineOptions);
+  };
+
+  makeColorButton = (color, title) => {
+    let { classes } = this.props
+     let result =  <Tooltip title={title} arrow>
+                      <Button variant="outlined" size="small" style={{borderColor: color, background: "white", }} data-color={color} ref={this.handleRef} className={`${classes.button} button-color`}>
+                        <ShowChartIcon style={{color: color}} />
+                      </Button>
+                  </Tooltip>
+
+    return result;
   };
 
   buildColorPalette = () => {
-    let { dataOptions, colorButtons } = this.state;
-    let colorPalette = document.getElementById("color-palette");
+    let { dataOptions } = this.state;
+    let elmButton = []
     dataOptions.map((data, index) => {
       let currColor = data.color;
-      let colorButton = this.makeColorButton(currColor);
-      colorPalette.appendChild(colorButton);
-      colorButtons[currColor] = colorButton;
+      let title = data.name
+      let colorButton = this.makeColorButton(currColor, title);
+
+      elmButton.push(React.createElement('span', null, colorButton));
+      
     });
 
-    this.handleSelectColor(dataOptions[0].color);
+
+    function ButtonSelected() {
+      return elmButton.map(data => data)
+    }
+    ReactDOM.render(<ButtonSelected />, document.getElementById('button-action'))
+
+    setTimeout(() => {
+      this.handleSelectColor(dataOptions[0].color);
+    }, 1000)
   };
 
   handleDeleteShapeSelected = () => {
-    let { shapeSelected } = this.state
-    let data = JSON.parse(localStorage.getItem("dataShape"))
+    let { shapeSelected, dataShape } = this.state;
+    if (shapeSelected) {
+      console.log("shapeSelected", shapeSelected);
+      let newData = dataShape.filter((shape) => shape.id !== shapeSelected.id);
 
-    if(shapeSelected) {
-      console.log('shapeSelected', shapeSelected)
-      let newData = data.filter(shape => shape.color !== shapeSelected.strokeColor)
-      localStorage.setItem("dataShape", JSON.stringify(newData))
-      shapeSelected.setMap(null)
+      this.setState({
+        ...this.state,
+        dataShape: newData,
+      });
+
+      localStorage.setItem("dataShape", JSON.stringify(newData));
+      shapeSelected.setMap(null);
     }
-  }
+  };
+
+  handleEditableShape = (shape, dataShape) => {
+    let paths = this.handleParsePaths(shape);
+
+    if (dataShape.length) {
+      dataShape.map((data) => {
+        if (data.id === shape.id) {
+          data.path = paths;
+        }
+      });
+    }
+
+    localStorage.setItem("dataShape", JSON.stringify(dataShape));
+  };
+
+  handleParsePaths = (type, shape) => {
+    let paths = [];
+
+    if(type === 'polyline') {
+      let pathArray = shape.getPath().i;
+      pathArray.forEach(function (path) {
+        paths.push({ lat: path.lat(), lng: path.lng() });
+      });
+
+    }
+    if(type === 'circle') {
+      paths.push({ lat: shape.center.lat(), lng: shape.center.lng() })
+    }
+    
+    return paths;
+  };
+
+  randomID = () => {
+    return Math.random()
+      .toString(36)
+      .replace(/[^a-z]+/g, "")
+      .substr(0, 10);
+  };
 
   render() {
     const { classes } = this.props;
     return (
       <div>
         <div id="panel" className={classes.panel}>
-          <div id="color-palette"></div>
+          <div id="button-action" className={classes.boxButton}></div>
           <div>
-            <button id="delete-button" className={classes.btnRemove}>Delete Selected Shape</button>
+            <Button id="delete-button" variant="contained"> Xóa 1 đường vẽ </Button>
           </div>
         </div>
         <div id="map" className={classes.root}></div>
